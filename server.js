f98e650281d93a0d84432bf0e59438008a83447d
Flávio Medeiros <flaviomotamedeiros@gmail.com>
2026-08-12 11:47:31 -0300

    Resolução do exercício

--- server.js
+++ server.js
@@ -6,4 +6,15 @@ const app = express();
 //     /busca?q=js&pagina=2   → { "q": "js", "pagina": "2" }
 //     /busca (sem parâmetros) → { "q": "" }
 
+app.get('/busca', (req, res) => {
+    let params = req.query;
+
+    // Se não tivermos parâmetros
+    if (Object.keys(params).length == 0){
+        res.json({'q': ''});
+    }
+
+    res.json(params);
+})
+
 app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
